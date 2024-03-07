f98a92759eaf8d42c61db6788cc3cf62ec951046
Joep van der Heijden <jvdheijden90@gmail.com>
2024-03-07 11:23:58 +0100

    put back the this context

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ const memoizePromiseFn = <T extends (...args: any[]) => Promise<any>>(fn: T) => 
   const cache = new Map<string, ReturnType<T>>();
 
   return (...args: Parameters<T>): ReturnType<T> => {
+    const context = this;
     const key = JSON.stringify(args);
 
     if (cache.has(key)) {
       return cache.get(key)!;
     }
 
-    const resultPromise = fn(...args).catch((error) => {
+    const resultPromise = fn.call(context, ...args).catch((error) => {
       // Delete cache entry if promise rejection occurs
       cache.delete(key);
       return Promise.reject(error);
